6871e7d83d52333c6eaa86352ef56a30bf624689
stanoira <smartineztanoira@itba.edu.ar>
2022-05-13 17:59:16 -0300

    started code

---
+++ MQTTListener2.cpp
@@ -0,0 +1,11 @@
+#include "MQTTListener2.h"
+
+void MQTTListener2::onMessage(std::string topic, std::vector<char> payload)
+{
+    if(topic == "ball/motion/state")
+    {
+        message = 
+    }
+}
+
+
--- main.cpp
+++ main.cpp
@@ -11,13 +11,21 @@
  * Main module
  */
 #include <iostream>
+#include <raylib.h>
+#include "MQTTClient2.h"
+#include "MQTTListener2.h"
 
 using namespace std;
 
 int main(void)
 {
-    while (true)
-    {
+    MQTTClient2 robot;
+    robot.connect("robot1.1", "127.0.0.1", 1883, "user", "vdivEMMN3SQWX2Ez");
+    if (robot.isConnected())
+        cout << "hola" << endl;
+    robot.subscribe("ball/motion/state");
+    
+    MQTTListener2 listener;
+    robot.setListener(&listener);
 
-    }
 }
